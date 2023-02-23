65db578c04957d4e717b21247ad59dcc7802417d
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-02-23 21:01:18 +0100

    23.02

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Something';
+let age;
+let ids = ['1', '2', '3', '4', '5'];
+ids.push('6');
+let arr = [false, 'Hello', 54, null, undefined, ['buraki', 'kartofle'], { name: 'Radek', age: 23 }];
+// Tuple
+const person = [1, 'Radek', true];
+// Tuple Array
+let employee = [
+    [1, 'Brad', true],
+    [2, 'John', false],
+    [3, 'Sasha', true],
+];
+for (let i = 0; i < employee.length; i++) {
+    console.log(employee[i]);
+}
+// Union
+const identifier = 22;
+// Enum
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
+// Basic Types
 let id: number = 5;
+let company: string = 'Traversy Media';
+let isPublished: boolean = true;
+let x: any = 'Something';
+let age: number;
+
+let ids: string[] = ['1', '2', '3', '4', '5'];
+ids.push('6');
+let arr: any[] = [false, 'Hello', 54, null, undefined, ['buraki', 'kartofle'], {name: 'Radek', age: 23}];
+
+// Tuple
+const person: [number, string, boolean] = [1, 'Radek', true];
+
+// Tuple Array
+let employee: [number, string, boolean][] = [
+    [1, 'Brad', true],
+    [2, 'John', false],
+    [3, 'Sasha', true],
+]
+
+for (let i = 0; i < employee.length; i++) {
+    console.log(employee[i]);
+}
+
+// Union
+const identifier: string | number = 22;
+
+// Enum
