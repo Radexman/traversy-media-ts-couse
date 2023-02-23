f79b17d3d4a529c626374e20dab4f08d144d00e7
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-02-23 21:33:38 +0100

    23.02

--- dist/index.js
+++ dist/index.js
@@ -22,3 +22,39 @@ for (let i = 0; i < employee.length; i++) {
 // Union
 const identifier = 22;
 // Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+;
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const BMW = {
+    brand: 'BMW',
+    prodYear: 2007,
+    isElectric: false,
+    owner: {
+        name: 'Emilia',
+        age: 20,
+    }
+};
+console.log(BMW);
+const { brand, prodYear, isElectric, } = BMW;
+console.log(brand, prodYear, isElectric);
+// Type Assertion
+let cid = 1;
+// let customerId = <string>cid;
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(5, 10));
--- src/index.ts
+++ src/index.ts
@@ -27,3 +27,68 @@ for (let i = 0; i < employee.length; i++) {
 const identifier: string | number = 22;
 
 // Enum
+enum Direction1 {
+   Up = 1,
+   Down,
+   Left,
+   Right, 
+};
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right',
+}
+
+// Objects
+
+// type User = {
+//     id: number,
+//     name: string,
+// }
+
+// const user: User = {
+//     id: 1,
+//     name: 'John',
+// }
+
+type Car = {
+    brand: string,
+    prodYear: number,
+    isElectric: boolean,
+    owner: {name: string, age: number},
+};
+
+const BMW: Car = {
+    brand: 'BMW',
+    prodYear: 2007,
+    isElectric: false,
+    owner: {
+        name: 'Emilia',
+        age: 20,
+    }
+}
+
+console.log(BMW);
+const {
+    brand,
+    prodYear,
+    isElectric,
+} = BMW;
+
+console.log(brand, prodYear, isElectric);
+
+// Type Assertion
+
+let cid: any = 1;
+// let customerId = <string>cid;
+
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+    return x + y;
+}
+
+console.log(addNum(5, 10));
